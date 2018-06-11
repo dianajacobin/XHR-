794afa2db7a91b871bbbe10e07e753030a69b0cb
dianajacobin <dianajacobin@gmail.com>
2018-06-11 17:25:59 -0500

    Creando archivos para prueba de XHR

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('search-form'); // contiene el formulario
+const searchField = document.getElementById('search-keyword'); //input
+const responseContainer = document.getElementById('response-container'); //boton
+let searchedForText;
+
+
+form.addEventListener('submit', e =>{
+ e.preventDefault();
+ responseContainer.innerHTML=' ';
+ searchdForText= searchField.value;
+ getNews();
+}); 
+//crearemos las peticiones
+ getNews = () => {
+    const articleRequest=new XMLHttpRequest();                                                                                                                                                                                            
+    articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=`);
+    articleRequest.onload = addNews;
+    articleRequest.onerror=handleError;
+    articleRequest.send();
+}
+
+ handleError = () =>  console.log ('Se ha presentado un error');
+
+ addNews = () =>{
+     const data=JSON.parse(this.responseText);
+     console.log(data);
+    // const response = data.response;
+    // console.log(response);
+ }
+
+
