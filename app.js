2aa2d61fad6b19e0dbe91e56b29576b8d21cd64e
dianajacobin <dianajacobin@gmail.com>
2018-06-11 19:39:47 -0500

    Elimino comentarios de JS

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const responseContainer = document.getElementById('response-container'); //boton
 let searchedForText;
 
 
-form.addEventListener('submit', e =>{
+form.addEventListener('submit', function (e) {
  e.preventDefault();
  responseContainer.innerHTML=' ';
  searchdForText= searchField.value;
  getNews();
 }); 
 //crearemos las peticiones
- getNews = () => {
+ function getNews () {
     const articleRequest=new XMLHttpRequest();                                                                                                                                                                                            
     articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=06a969bb2a8947fc85ec2500771f4f8d`);
     articleRequest.onload = addNews;
@@ -19,13 +19,12 @@ form.addEventListener('submit', e =>{
     articleRequest.send();
 }
 
- handleError = () =>  console.log ('Se ha presentado un error');
+ function handleError () { console.log ('Se ha presentado un error');}
 
- addNews = () =>{
+ function addNews () {
      const data=JSON.parse(this.responseText);
      console.log(data);
-    // const response = data.response;
-    // console.log(response);
+ 
  }
 
 
